5acb7654325590edd86d5d95781c7c9d808a6be3
Caen De Silva <caen@desilva.se>
2024-05-06 21:50:22 +0200

    Merge in static extension list
    Not all were found by the extension checker call

--- src/run.php
+++ src/run.php
@@ -17,7 +17,9 @@ function createFunctionDataset(): void
     $functions = $functions['internal'];
 
     // Remove functions that are from extensions
-    $extensions = get_loaded_extensions();
+    $loadedExtensions = get_loaded_extensions();
+    $staticExtensions = ['datefmt', 'finfo', 'intlcal', 'intltz', 'mb', 'msgfmt', 'numfmt', 'transliterator'];
+    $extensions = array_merge($loadedExtensions, $staticExtensions);
     $prefixes = array_map(fn (string $extension): string => $extension . '_', $extensions);
     $functions = array_filter($functions, fn (string $function) => ! str_starts_with_any($function, $prefixes));
 
