ec6aadefd112b25cd12e1ffead48bf76a35b9e52
Caen De Silva <caen@desilva.se>
2024-05-06 21:30:18 +0200

    Sketch out base console status

--- src/run.php
+++ src/run.php
@@ -1,1 +1,9 @@
 <?php
+
+$timeStart = microtime(true);
+
+echo "Generating datasets...\n";
+
+// Do stuff
+
+echo "Done! (Completed in " . number_format((microtime(true) - $timeStart) * 1000, 2) . "ms)\n";
