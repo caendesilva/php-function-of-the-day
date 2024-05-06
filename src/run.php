3473a17623469db7035eefa6f2cfd62e34de7c87
Caen De Silva <caen@desilva.se>
2024-05-06 22:17:25 +0200

    Parse function indexes

--- src/run.php
+++ src/run.php
@@ -6,6 +6,7 @@ echo "Generating datasets...\n";
 
 createFunctionDataset();
 downloadFunctionIndexes();
+parseFunctionIndexes();
 
 echo "Done! (Completed in " . number_format((microtime(true) - $timeStart) * 1000, 2) . "ms)\n";
 
@@ -38,6 +39,51 @@ function downloadFunctionIndexes(): void
     }
 
     assert(filesize($cacheFile) > 1024);
+}
+
+function parseFunctionIndexes(): void
+{
+    $cacheDir = __DIR__ . '/../data/cache';
+    $cacheFile = $cacheDir . '/manual.indexes.functions.php.html';
+
+    $html = file_get_contents($cacheFile);
+
+    $start = '<h2 class="title">Function and Method listing</h2>';
+    $end = '<h3 class="title">Improve This Page</h3>';
+    $html = substr($html, strpos($html, $start) + strlen($start));
+    $html = substr($html, 0, strpos($html, $end));
+
+    // Remove newlines inside <li> and </li> tags
+    $html = preg_replace('/(<li>.*?)(\R+)(.*?<\/li>)/s', '$1 $3', $html);
+
+    $lines = explode("\n", $html);
+
+    $relevantLines = [];
+    foreach ($lines as $line) {
+        if (str_contains($line, 'class="index"')) {
+            $relevantLines[] = $line;
+        }
+    }
+
+    $functions = [];
+    foreach ($relevantLines as $line) {
+        // example line: <li><a href='function.abs.php' class='index'>abs</a> - Absolute value</li>
+        preg_match('/<li><a href="(.*?)" class="index">(.*?)<\/a> - (.*?)<\/li>/', $line, $matches);
+
+        if (count($matches) !== 4) {
+            throw new Exception('Invalid match count: ' . count($matches) . ' for line: ' . $line);
+        }
+
+        $functions[] = [
+            'name' => $matches[2] ?: null,
+            'url' => $matches[1] ?: null,
+            'description' => $matches[3] ?: null,
+        ];
+    }
+
+    assert(count($functions) > 1000);
+
+    file_put_contents(__DIR__ . '/../data/php-functions-indexes.json', json_encode($functions, JSON_PRETTY_PRINT));
 }
 
 // Helper functions
