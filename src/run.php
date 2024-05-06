db8ed813e5c4a98ca2f1adbbf0c99cd0932fb764
Caen De Silva <caen@desilva.se>
2024-05-06 21:32:58 +0200

    Create function dataset

--- src/run.php
+++ src/run.php
@@ -4,6 +4,17 @@ $timeStart = microtime(true);
 
 echo "Generating datasets...\n";
 
-// Do stuff
+createFunctionDataset();
 
 echo "Done! (Completed in " . number_format((microtime(true) - $timeStart) * 1000, 2) . "ms)\n";
+
+function createFunctionDataset()
+{
+    // Get all defined functions
+    $functions = get_defined_functions();
+
+    // Use only internal functions
+    $functions = $functions['internal'];
+
+    file_put_contents(__DIR__ . '/../data/php-functions.txt', implode("\n", $functions));
+}
